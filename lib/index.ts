cb0227bf446919b1da145307d4edba48c4df7f2a
seveibar <seveibar@gmail.com>
2025-06-21 21:15:21 -0700

    init project

---
+++ lib/index.ts
@@ -0,0 +1,22 @@
+export interface ElbowPoint {
+  x: number
+  y: number
+  facingDirection?: "x+" | "x-" | "y+" | "y-"
+}
+
+export const calculateElbow = (
+  point1: ElbowPoint,
+  point2: ElbowPoint,
+  options: {
+    /**
+     * Amount to overshoot the line if the facingDirection requires that we go
+     * beyond "out" before turning
+     */
+    overshoot?: number
+  } = {},
+): Array<{ x: number; y: number }> => {
+  const overshoot =
+    options?.overshoot ??
+    0.1 * Math.max(Math.abs(point1.x - point2.x), Math.abs(point1.y - point2.y))
+  // TODO
+}
